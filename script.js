81c61dd48a509941446e83a84c421468d68d2fc9
Snow <singingai@hotmail.com>
2020-01-24 22:38:12 -0600

    1

---
+++ script.js
@@ -0,0 +1,25 @@
+var number = 3;
+var string = "I believe it to be fine.";
+
+// Operators
+var s = 2;
+var n = 2;
+
+var sum = s + n;
+s == n;
+
+// If Statement
+var o = 6;
+var w = 2;
+var theNumbersMatch
+
+if ( 0 == w ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log(theNumbersMatch);
+
+// Alert
+alert("Time to go to the tea party!");

b09cadb0610aa075d7e4746e902288250fe3bee1
Snow <singingai@hotmail.com>
2020-02-21 22:46:20 -0600

    3

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+//getElementByID//
 
-// Switch //
+var myDemo = document.getElementById('bar');
 
+console.log(myDemo.childNodes.length);
+
+var myDemo = document.getElementById('DVD');
+
+console.log(myDemo.childNodes.length);
+
+//getElementByTag//
+
+var Tags = document.getElementsByTagName('li');
+
+console.log ("Number of li links: "+ Tags.length);
+
+var LinksMenu = myDemo.getElementsByTagName('li');
+
+console.log ("Number of li in menu: "+ LinksMenu.length);
+
+//createElement + appendChild//
+var item = document.createElement("li");
+myDemo.appendChild(item);
+
+var lvl = document.createElement("h3");
+myDemo.appendChild(lvl);
+
+//createTextNode + appendChild//
+
+var text = document.createTextNode("Unknown world");
+item.appendChild(text);
+
+var type = document.createTextNode("Help");
+lvl.appendChild(type);
+
+//Extra//
 var personName = prompt ("What is your name?");
 
 switch(personName) {
@@ -17,13 +50,13 @@ switch(personName) {
 
 }
 
-// Function //
+//Release Date//
 
-function addNumbers(s, n) {
-var addedTotal= s +n;
+function addNumbers(r, d) {
+var addedTotal= r +d;
 return addedTotal;
 
 }
 
-var add2 = addNumbers(1,10);
+var add2 = addNumbers(9,20);
 alert (add2);
